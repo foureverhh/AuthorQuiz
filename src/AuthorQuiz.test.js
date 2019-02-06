594cd8f2d5871c2c1be2ef03e08dc2b31d101fb2
foureverhh <foureverhh@gmail.com>
2019-02-06 14:02:54 +0100

    enzyme test with wrapper and Object.assign

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthorQuiz from './AuthorQuiz';
-//import Enzyme,{ shallow } from 'enzyme';
-//import Adapter from 'enzyme-adapter-react-16';
+import Enzyme,{ mount,shallow,render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({adapter: new Adapter()});
+
+const state = {
+  turnData: {
+    books:['The shining','IT','David Copperfield','A Tale of Two Cities'],
+    author: {
+      name: 'Charles Dickens',
+      imageUrl: 'images/authors/charlesdickens.jpg',
+      imageSource: 'Wikipedia Commons',
+      books:['David Copperfield','A Tale of Two Cities']
+    }
+  },
+  highlight: 'none'
+}
+
 
 describe('Author Quiz',()=>{
   it('render without crashing',()=>{
       const div = document.createElement('div');
-      ReactDOM.render(<AuthorQuiz />,div);
+      ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={()=>{}}/>,div);
   });
+
+  describe("When no answer has been selected", ()=>{
+    let wrapper;
+
+    beforeAll(()=>{
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={()=>{}} />);
+    });
+
+    it("should have no background color",()=>{
+      expect(wrapper.find('div.row.turn').props().style.backgroundColor).toBe('');
+    });
+  });
+
+  describe('when correct answer has been selected', ()=>{
+    let wrapper;
+    beforeAll(()=>{
+      wrapper=mount(<AuthorQuiz {...(Object.assign({},state,{highlight:'correct'}))} onAnswerSelected={()=>{}}/>);
+    });
+    it('should have a green background color',()=>{
+      expect(wrapper.find('div.row.turn').props().style.backgroundColor).toBe('green');
+    });
+  });
+
+  describe("When wrong answer has been selected", ()=>{
+    let wrapper;
+    beforeAll(()=>{
+      wrapper=mount(<AuthorQuiz {...(Object.assign({},state,{highlight:'wrong'}))} onAnswerSelected={()=>{}} />);
+    });
+    it("should have a red background color",()=>{
+      expect(wrapper.find('div.row.turn').props().style.backgroundColor).toBe('red');
+    });
+  });
+
+
+
 });
+
